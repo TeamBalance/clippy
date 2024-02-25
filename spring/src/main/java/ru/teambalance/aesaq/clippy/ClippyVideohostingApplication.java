859a143aeee4a841c0d6805cfc9c274614c98ce5
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-02-25 18:16:02 +0500

    Initial project setup
    Set up a new Spring Boot project with basic configurations

---
+++ spring/src/main/java/ru/teambalance/aesaq/clippy/ClippyVideohostingApplication.java
@@ -0,0 +1,13 @@
+package ru.teambalance.aesaq.clippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClippyVideohostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClippyVideohostingApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/ru/teambalance/aesaq/clippy/ClippyVideohostingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.teambalance.aesaq.clippy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClippyVideohostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
